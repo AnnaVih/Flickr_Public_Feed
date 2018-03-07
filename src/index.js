209cf49d674a3f937256f102b1bc1f5a3342eeba
Anna Vihrogonova <annavihrogonova@yahoo.com>
2018-03-07 19:55:18 +0000

    Import Bootstrap 4 into style.scss file and import both of them inside of index.js. Now we can easily use all Bootstrap  and sass features. And compile sass to plain css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+'use strict';
+
+require('bootstrap');
+
 require('./scss/style.scss');
+
+//Importing classes
+import { GetPosts } from './fetchData';
+
+
+/***************  GETTING POSTS  ************************
+ *************************************************************/
+
+function getPhotoPosts() {
+    console.log('hi');
+    //1. Make instance of GetPosts
+    const photoPosts = new GetPosts();
+
+    //2.Getting data from promises
+    photoPosts.get()
+              .then((postsResult) => {
+                  console.log(postsResult);
+              })
+              //4.1 Catch error in case if no weather data recieved
+              .catch((err) => {
+                console.log(err);
+            });
+}
+
+
+/******************  EVENT LISTENERS  ***********************
+ ***********************************************************/
+
+//On load Event listeners
+document.addEventListener("DOMContentLoaded", getPhotoPosts);
